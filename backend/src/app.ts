51dcf487ee6c88f00c8d496bba4e304803f03515
dev-chandan-pandey <iamchandanpandey11@gmail.com>
2026-02-04 13:51:21 +0530

    env fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "https://chatterbox-ai-tidm.vercel.app", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://chatterbox-ai-tidm.vercel.app"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
